96d295f1e668e958fc8e0ebae9e962a82ed804f9
trungtashi99 <trungtashi@gmai.com>
2022-06-07 10:25:18 +0700

    Sử dụng toán tử

---
+++ script.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+    inputHeight = prompt("Nhập chiều cao")
+    inputWidth = prompt("Nhập chiều dài")
+
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+let area = width * height
+
+document.write('Diện tích = '+area)
